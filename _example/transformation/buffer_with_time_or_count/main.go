80943542b7db40f019159dda3c4c93bfd02cdb98
Sutee <sutee.s@linecorp.com>
2025-09-02 20:18:48 +0700

    feat: add BufferWithTime and BUfferWithTimeOrCount

---
+++ _example/transformation/buffer_with_time_or_count/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/foreveraloneT/trx/op"
+)
+
+func main() {
+	basicUsage()
+}
+
+func basicUsage() {
+	fmt.Println("Basic Usage:")
+
+	source := op.Take(op.Interval(200*time.Millisecond), 10)
+
+	out := op.BufferWithTimeOrCount(source, 1*time.Second, 3)
+
+	for val := range out {
+		v, err := val.Get()
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("Buffered: %v\n", v)
+	}
+}
